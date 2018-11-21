abee710e3027352947539c38f0632eef3154d925
Justin Holt <justinvholt@gmail.com>
2018-11-21 13:20:18 -0200

    Scaffolded restaurants, created review model and controller, inserted validations, connected everything in router

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+# model for all restaurant instances
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :phone_number, uniqueness: true, presence: true
+  validates :category, presence: true, inclusion: { in: %w[chinese italian japanese french belgian] }
+end
---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+# model for all review instances
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: 0..5 }
+end
